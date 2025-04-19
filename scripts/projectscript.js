c71ab76951f70dd9644ba0d9c7660192e2635c01
Debanik Pahilwani <pahilwanidebanik@gmail.com>
2025-04-19 16:56:26 +0530

    started projects and experience

---
+++ scripts/projectscript.js
@@ -0,0 +1,15 @@
+const projects=document.getElementById('projects')
+
+projects.style.display='flex'
+projects.style.flexDirection='row'
+projects.style.gap=10
+projects.style.flexWrap='wrap'
+
+
+const projjson=[
+    {
+        name: '',
+        description:'',
+        image:''
+    }
+]
